fc8174b89386b913aaa1798ddfed03033348fd4f
jung su young <j9972@naver.com>
2021-04-10 01:33:08 +0900

    solve the error

--- server/index.js
+++ server/index.js
@@ -8,42 +8,11 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    user: 'root',
+    user: 'pedro',
     host: 'localhost',
     password: 'password', // or ''
     database: 'CRUDDatabase',
 });
-
-// const handleDisconnect = () => {
-//     const connection = mysql.createConnection(db);
-
-//     connection.connect((err) => {
-//         if(err) {
-//             console.log('error whenn connection to db:', err);
-//             setTimeout(handleDisconnect, 2000);
-//         }
-//     });
-
-//     connection.on('error', (err) => {
-//         console.log('db error', err);
-//         if(err.code === 'PROTOCO_CONNECTION_LOST') {
-//             handleDisconnect();
-//         } else {
-//             throw err;
-//         }
-//     })
-// }
-
-// handleDisconnect();
-
-
-// db.query('SELECT * FROM CRUDDatabase', (error,results, fields) => {
-//     if(error) {
-//         console.log(error);
-//     }
-//     console.log(results, 'shits');
-// });
-
 
 app.post('/create', (req,res) => {
     const name = req.body.name;
@@ -64,7 +33,7 @@ app.post('/create', (req,res) => {
     }); 
 });
 
-//app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.listen(3003, () => {
     console.log("running on 3003");
