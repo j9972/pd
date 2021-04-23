e0bfaadd96057bc9408e18e1241f7feb79d9dd6b
jung su young <j9972@naver.com>
2021-04-23 11:24:33 +0900

    fix one of problem and add some code on server file

--- pedroBlogServer/server.js
+++ pedroBlogServer/server.js
@@ -1,18 +1,22 @@
-const bodyParser = require("body-parser");
 const express = require("express");
 const cors = require('cors');
-const app = express();
 const mysql = require('mysql');
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = 3001;
 
 app.use(cors());
 app.use(express.json());
 
-
+app.get("/", (req,res) => {
+    res.send("hi");
+})
 
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.listen(3001, () => {
-    console.log("running on 3003");
+app.listen(PORT, () => {
+    console.log(`running on ${PORT}`);
 });
 
 
