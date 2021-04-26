3f23d000914c49f1f51a25dc9c74164999e2e548
jung su young <j9972@naver.com>
2021-04-27 00:10:26 +0900

    JSON.parse(JSON.stringfy())

--- pedroBlogServer/server.js
+++ pedroBlogServer/server.js
@@ -7,24 +7,31 @@ const db = require("./config/db");
 const app = express();
 const PORT = 3004;
 
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/get", (req,res) => {
+app.get('/api/get', (req,res) => {
+    
     db.query("SELECT * FROM posts",
     (err,result) => {
         if(err) {
             console.log(err, 'err');
-        } 
-        res.send(result, 'success');
-    } );
-}) 
+        }
+        //res.send(result, 'success'); -> 이러면 code: 'ERR_HTTP_INVALIDE_STATUS_CODE" 에러뜸, + nodemon app crashing뜸.
+        const asult = {'result' : result}
+        res.status(200).send(JSON.parse(JSON.stringify(asult)));
+        console.log(result, '1_success');
+    });
+});
 
 app.post('/api/create', (req,res) => {
 
     const userName = req.body.userName;
     const title = req.body.title;
     const text = req.body.text;
+    
 
     db.query("INSERT INTO posts (title, post_text, user_name) VALUES (?,?,?)",
     [title, text, userName],
@@ -32,12 +39,10 @@ app.post('/api/create', (req,res) => {
         if(err) {
             console.log(err, 'err');
         } else {
-            console.log(result, 'success');
+            console.log(result, '2_success');
         }
     } );
 })
-
-app.use(bodyParser.urlencoded({extended:true}));
 
 app.listen(PORT, () => {
     console.log(`running on ${PORT}`);
